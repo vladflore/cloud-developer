801e5515066b2a0762b2039a19b50d27b8523f78
Vlad Flore <flore.vlad@gmail.com>
2020-12-09 16:16:58 +0100

    Fetch a feed by id and config aws rds

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagramflore",
+    "password": "udagramflore",
+    "database": "udagramflore",
+    "host": "udagramfloredev.ce8p30yx9faw.us-east-2.rds.amazonaws.com",
     "dialect": "postgres",
     "aws_region": "us-east-2",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagramflore-dev"
   },
   "prod": {
     "username": "",
